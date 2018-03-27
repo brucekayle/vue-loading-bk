f5b86848b610915ef288cac3df6f54a39685502b
BruceKayle <brucekayle@163.com>
2018-03-27 14:42:52 +0800

    first update

---
+++ demo/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="animated fadeIn">
+    <Loading :size="80" v-show="true"></Loading>
+  </div>
+</template>
+
+<script>
+import Loading from '../src/Loading.vue'
+export default {
+  name: 'dashboard',
+  components: {
+    Loading
+  }
+}
+</script>
+<style>
+
+</style>
